8c531824ab443c6f14d170455685946f2202a99d
Dhyanesh369 <k.r.dhyan9894@gmail.com>
2026-05-15 15:29:38 +0530

    Initial commit: TES-OS Modular System

---
+++ src/components/three/Scene.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import React, { useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { OrbitControls, PerspectiveCamera, Environment } from "@react-three/drei";
+import * as THREE from "three";
+
+const Box = () => {
+  const meshRef = useRef<THREE.Mesh>(null);
+
+  useFrame((state, delta) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += delta * 0.5;
+      meshRef.current.rotation.y += delta * 0.2;
+    }
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="royalblue" />
+    </mesh>
+  );
+};
+
+export const Scene = () => {
+  return (
+    <div className="h-[400px] w-full rounded-xl overflow-hidden border bg-muted/20">
+      <Canvas shadows>
+        <PerspectiveCamera makeDefault position={[0, 0, 5]} />
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <Box />
+        <Environment preset="city" />
+      </Canvas>
+    </div>
+  );
+};
